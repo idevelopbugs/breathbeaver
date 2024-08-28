98780b6cdc1e501b80932844f3ba7e6b1be1605c
Blackwinged <tasteofchains@gmail.com>
2024-08-28 19:56:32 +0200

    Fix path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/breathbeaver',
+  assetPrefix: '/breathbeaver/',
   output: 'export',
   reactStrictMode: true,
   eslint: {
